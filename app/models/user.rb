80e94415dbfadf7237e0f8dd84e9cf6037a2ae77
kotakamiya <118578397+kotakamiya@users.noreply.github.com>
2023-02-01 11:48:04 +0900

    新規登録に関するバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_kana, presence: true
-  validates :last_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: '全角カタカナを使用してください' } do
+    validates :first_kana, presence: true
+    validates :last_kana, presence: true
+  end
   validates :birth_day, presence: true
 
   has_many :items
