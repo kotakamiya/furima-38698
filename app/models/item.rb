049ad75b31b663d2904c00bba691dc9de9ef20fa
kotakamiya <118578397+kotakamiya@users.noreply.github.com>
2023-02-04 11:47:56 +0900

    Action Hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping
+  belongs_to :area
+  belongs_to :number_of_day
+
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :state_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :number_of_day_id, numericality: { other_than: 1, message: "can't be blank"}
+
 
   belongs_to :user
   has_one :item
