c031ae4b727433010ae6016da3c062e40d518d4b
kotakamiya <118578397+kotakamiya@users.noreply.github.com>
2023-02-07 11:04:27 +0900

    #5-商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,15 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :number_of_day
 
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :explain, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :state_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :shipping_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :area_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :number_of_day_id, numericality: { other_than: 1, message: "can't be blank"}
-
-
+  validates :price,numericality: { other_than: 1, message: "can't be blank"}, presence: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   belongs_to :user
   has_one :item
   has_one_attached :image
--- app/models/number_of_day.rb
+++ app/models/number_of_day.rb
@@ -1,12 +1,12 @@
-class Number_of_day < ActiveHash::Base
+class NumberOfDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '新品・未使用' },
-    { id: 3, name: '未使用に近い' },
-    { id: 4, name: '目立った傷や汚れなし' }
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '3~4日で発送' },
+    { id: 4, name: '4~7日で発送' }
   ]
   
   include ActiveHash::Associations
   has_many :items
   
-  end
+end
