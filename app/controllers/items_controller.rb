d1aa8177bab3a52b186f241f551071b955cb029f
kotakamiya <118578397+kotakamiya@users.noreply.github.com>
2023-02-12 16:47:05 +0900

    Revert "#7商品詳細ページの実装"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -17,10 +17,6 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
-
-  def show
-    @item = Item.find(params[:id])
-  end  
 
   private
   def item_params
